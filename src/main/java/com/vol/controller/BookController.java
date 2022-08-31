ef505cf6246ab90b4e97db24c489ee080db26fcd
volx <hastingsvol@163.com>
2022-08-31 20:48:09 +0800

    完成书籍查询

--- src/main/java/com/vol/controller/BookController.java
+++ src/main/java/com/vol/controller/BookController.java
@@ -1,19 +1,13 @@
 package com.vol.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
-
     @GetMapping("/id={id}")
     public String getBook(@PathVariable int id) {
         System.out.println("今天又摸鱼了，真好");
