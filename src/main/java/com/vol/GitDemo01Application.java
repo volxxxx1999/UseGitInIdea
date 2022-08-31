662fff515fa3396dc6eba0e4a743424b79e54ba3
volx <hastingsvol@163.com>
2022-08-31 14:11:30 +0800

    第三次提交

---
+++ src/main/java/com/vol/GitDemo01Application.java
@@ -0,0 +1,13 @@
+package com.vol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo01Application.class, args);
+    }
+
+}
